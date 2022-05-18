d56af44cf9f4382e1924108cd35a7c3dbb50c542
shamsiyakhan <sehrozkhan2704@gmail.com>
2022-05-18 13:10:18 +0530

    project

---
+++ chefarshadalam/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'chefarshadalam';
+
+/* arr=["https://qcexclusive.com/wp-content/uploads/2016/08/topchef-770x471.jpg","https://norwalk.edu/wp-content/uploads/2018/09/Chief-image-2.jpg"]
+a=this.arr[0]
+next(){
+
+    if(this.a==this.arr[0]){
+
+          this.a=this.arr[1]
+    }
+    else{
+
+      this.a=this.arr[0]
+    }
+
+} */
+
+
+}
